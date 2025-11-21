f2a0e0c4de01f6d8e46da60892b3b90db11512f7
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-21 19:11:03 +0530

    adding navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const NavBar = () => {
+
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState(false);
+
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Projects" },
+    { id: "education", label: "Education" },
+  ];
+
   return (
-    <div>
-      
+    <div className='bg-transparent'>
+      <div className="text-white py-5 flex justify-between items-center" >
+        <div className="text-white py-5 flex justify-between items-center">
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Animesh</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Kumar</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={`cursor-pointer hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        </div>
+      </div>
     </div>
   )
 }
