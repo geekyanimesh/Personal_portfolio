7b72db61fd8d5a1bd5425934ce0fad48935051ea
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-25 20:07:11 +0530

    experience begins

--- src/constants.js
+++ src/constants.js
@@ -36,8 +36,7 @@ import linux from "./assets/tech_logo/linux.png";
 
 // // Experience Section Logo's
 import sa from "./assets/company_logo/sa.png";
-import codsoft from "./assets/company_logo/codsoft.png";
-
+import cod from './assets/company_logo/cod.png';
 // // Education Section Logo's
 // import glaLogo from './assets/education_logo/gla_logo.png';
 // import bsaLogo from './assets/education_logo/bsa_logo.png';
@@ -53,6 +52,7 @@ import codsoft from "./assets/company_logo/codsoft.png";
 // import cmLogo from './assets/work_logo/cm.png';
 // import imagesearchLogo from './assets/work_logo/image_search.png';
 // import removebgLogo from './assets/work_logo/remove_bg.png';
+import deltaLogo from './assets/project_logos/deltaLogo.png';
 
 export const SkillsInfo = [
   {
@@ -124,6 +124,22 @@ export const experiences = [
       "Redux",
     ],
   },
+  {
+    id: 0,
+    img: cod,
+    role: "JavsScript Developer",
+    company: "CodSoft Inc",
+    date: "June 2024 - July 2024",
+    desc: "Successfully completed a 1-month internship as a JavaScript Developer, designing and developing projects including a digital clock, a personal portfolio website, and a to-do list application. Enhanced proficiency in JavaScript, DOM operations, and responsive design while applying best practices in front-end development. Improved real-world coding discipline, version control usage, and UI enhancement through iterative development.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Node JS",
+      "Tailwind CSS",
+    ],
+  },
 ];
 
 // export const education = [
@@ -165,95 +181,35 @@ export const experiences = [
 //   },
 // ];
 
-// export const projects = [
-//   {
-//     id: 0,
-//     title: "GitHub Profile Detective",
-//     description:
-//       "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
-//     image: githubdetLogo,
-//     tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
-//     github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
-//     webapp: "https://githubprofiledetective.netlify.app/",
-//   },
-//   {
-//     id: 1,
-//     title: "CS Prep",
-//     description:
-//       "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
-//     image: csprepLogo,
-//     tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
-//     github: "https://github.com/codingmastr/CSPrep",
-//     webapp: "https://csprep.netlify.app/",
-//   },
-//   {
-//     id: 2,
-//     title: "Movie Recommendation App",
-//     description:
-//       "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
-//     image: movierecLogo,
-//     tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-//     github: "https://github.com/codingmastr/Movie-Recommendation-App",
-//     webapp: "https://movie-recommendation-app-jet.vercel.app/",
-//   },
-//   {
-//     id: 3,
-//     title: "Email Validator NPM Package",
-//     description:
-//       "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
-//     image: npmLogo,
-//     tags: ["React JS", "Node.js", "NPM", "Validation"],
-//     github: "https://github.com/codingmastr/cmtk-email-validator",
-//     webapp: "https://www.npmjs.com/package/cmtk-email-validator",
-//   },
-//   {
-//     id: 4,
-//     title: "Task Reminder Chrome Extension Tool",
-//     description:
-//       "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
-//     image: taskremLogo,
-//     tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-//     github: "https://github.com/codingmastr/Task-Reminder-Tool",
-//     webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
-//   },
-//   {
-//     id: 5,
-//     title: "Webverse Digital",
-//     description:
-//       "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-//     image: webverLogo,
-//     tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-//     github: "https://github.com/codingmastr/Webverse-Digital",
-//     webapp: "https://webversedigital.com/",
-//   },
-//   {
-//     id: 6,
-//     title: "Coding Master",
-//     description:
-//       "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-//     image: cmLogo,
-//     tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-//     github: "https://codingmasterweb.in/",
-//     webapp: "https://codingmasterweb.in/",
-//   },
-//   {
-//     id: 7,
-//     title: "Image Search App",
-//     description:
-//       "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-//     image: imagesearchLogo,
-//     tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-//     github: "https://github.com/codingmastr/Image-Search-App",
-//     webapp: "https://imagsearch.netlify.app/",
-//   },
-//   {
-//     id: 8,
-//     title: "Image Background Remover",
-//     description:
-//       "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-//     image: removebgLogo,
-//     tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-//     github: "https://github.com/codingmastr/Image-Background-Remover",
-//     webapp: "https://removeyourbg.netlify.app/",
-//   },
-// ];
+ export const projects = [
+  {
+    id: 0,
+    title: "deltaLogo",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: deltaLogo,
+    tags: ["HTML", "TailwindCSS", "TypeScript", "React JS", "API"],
+    github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "प्रKriti",
+    description:
+      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    //image: प्रKriti,
+    tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/codingmastr/CSPrep",
+    webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "FeedMeBack",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    //image: 
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    webapp: "https://movie-recommendation-app-jet.vercel.app/",
+  },
+];
