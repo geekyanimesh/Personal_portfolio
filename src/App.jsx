962c158849860cdc31c7c4fb19dd977d1b73daf0
geekyanimesh <animesh.kumar9651@gmail.com>
2025-12-02 21:50:06 +0530

    education done

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,25 @@ import BlurBlob from './components/BlurBlob';
 
 function App() {
   return (
-    <div  className="bg-[#050414]">
-
-     <BlurBlob position={{ top: '40%', left: '30%' }} size={{ width: '30%', height: '40%' }} />
+    <div className="bg-[#050414]">
+      <BlurBlob
+        position={{ top: "40%", left: "30%" }}
+        size={{ width: "30%", height: "40%" }}
+      />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-        <div className='relative pt-20'>
-          <NavBar></NavBar>
-          <About></About>
-          <Skills></Skills>
-          <Experience></Experience>
-          <Work></Work>
-          <Education></Education>
-          <Contact></Contact>
-        </div>
+      <div className="relative pt-20">
+        <NavBar></NavBar>
+        <About></About>
+        <Skills></Skills>
+        <Experience></Experience>
+        <Work></Work>
+        <Education></Education>
+        <Contact></Contact>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/constants.js
+++ src/constants.js
@@ -33,26 +33,22 @@ import vercelLogo from "./assets/tech_logo/vercel.png";
 import postgreLogo from "./assets/tech_logo/postgre.png";
 import csharpLogo from "./assets/tech_logo/csharp.png";
 import linux from "./assets/tech_logo/linux.png";
+import dockerLogo from "./assets/tech_logo/dockerLogo.png";
+import kubernetesLogo from "./assets/tech_logo/kubernetesLogo.png"
 
 // // Experience Section Logo's
 import sa from "./assets/company_logo/sa.png";
 import cod from './assets/company_logo/cod.png';
+
 // // Education Section Logo's
-// import glaLogo from './assets/education_logo/gla_logo.png';
-// import bsaLogo from './assets/education_logo/bsa_logo.png';
-// import vpsLogo from './assets/education_logo/vps_logo.png';
+import BIT from "./assets/school_logo/BIT.png"
+import jvmLogo from "./assets/school_logo/jvmLogo.jpg"
+import mgm from './assets/school_logo/mgm.jpeg'
 
 // // Project Section Logo's
-// import githubdetLogo from './assets/work_logo/github_det.png';
-// import csprepLogo from './assets/work_logo/cs_prep.png';
-// import movierecLogo from './assets/work_logo/movie_rec.png';
-// import taskremLogo from './assets/work_logo/task_rem.png';
-// import npmLogo from './assets/work_logo/npm.png';
-// import webverLogo from './assets/work_logo/web_dig.png';
-// import cmLogo from './assets/work_logo/cm.png';
-// import imagesearchLogo from './assets/work_logo/image_search.png';
-// import removebgLogo from './assets/work_logo/remove_bg.png';
-import deltaLogo from './assets/project_logos/deltaLogo.png';
+import deltaAi from './assets/project_logos/deltaAi.png'
+import prakriti from './assets/project_logos/prakriti.png'
+import feedMeBack from './assets/project_logos/feedMeBack.png'
 
 export const SkillsInfo = [
   {
@@ -70,13 +66,15 @@ export const SkillsInfo = [
     ],
   },
   {
-    title: "Backend",
+    title: "Backend & DevOps",
     skills: [
       { name: "Node JS", logo: nodejsLogo },
       { name: "Express JS", logo: expressjsLogo },
       { name: "MySQL", logo: mysqlLogo },
       { name: "MongoDB", logo: mongodbLogo },
       { name: "Firebase", logo: firebaseLogo },
+      { name: "Docker", logo: dockerLogo },
+      { name: "Kubernetes", logo: kubernetesLogo },
     ],
   },
   {
@@ -142,74 +140,65 @@ export const experiences = [
   },
 ];
 
-// export const education = [
-//   {
-//     id: 0,
-//     img: glaLogo,
-//     school: "GLA University, Mathura",
-//     date: "Sept 2022 - July 2024",
-//     grade: "7.81 CGPA",
-//     desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-//     degree: "Master of Computer Applications - MCA",
-//   },
-//   {
-//     id: 1,
-//     img: bsaLogo,
-//     school: "BSA College, Mathura",
-//     date: "Sept 2018 - Aug 2021",
-//     grade: "73.2%",
-//     desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-//     degree: "Bachelor of Science - BSC (Computer Science)",
-//   },
-//   {
-//     id: 2,
-//     img: vpsLogo,
-//     school: "Vatsalya Public School Govardhan, Mathura",
-//     date: "Apr 2017 - March 2018",
-//     grade: "78%",
-//     desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-//     degree: "CBSE(XII) - PCM with Computer Science",
-//   },
-//   {
-//     id: 3,
-//     img: vpsLogo,
-//     school: "Vatsalya Public School Govardhan, Mathura",
-//     date: "Apr 2015 - March 2016",
-//     grade: "87.5%",
-//     desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-//     degree: "CBSE(X), Science with Computer Application",
-//   },
-// ];
+export const education = [
+  {
+    id: 0,
+    img: BIT,
+    school: "BIT Sindri",
+    date: "Sept 2023 - July 2027",
+    grade: "8.00 CGPA",
+    desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+    degree: "Bachelor of Technology, Computer Science Engineering",
+  },
+  {
+    id: 1,
+    img: jvmLogo,
+    school: "Jawahar Vidya Mandir, Shyamali",
+    date: "June 2021 - May 2023",
+    grade: "88.4%",
+    desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "CBSE (XII) - PCM with Computer Science",
+  },
+  {
+    id: 2,
+    img: mgm,
+    school: "MGM Higher Secondary School",
+    date: "Apr 2015 - April 2021",
+    grade: "96%",
+    desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    degree: "CBSE(X) ",
+  },
+];
 
  export const projects = [
   {
     id: 0,
-    title: "deltaLogo",
+    title: "deltaAi",
     description:
       "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
-    image: deltaLogo,
+    image: deltaAi,
     tags: ["HTML", "TailwindCSS", "TypeScript", "React JS", "API"],
-    github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    github: "https://github.com/geekyanimesh/delta",
     webapp: "https://githubprofiledetective.netlify.app/",
   },
   {
     id: 1,
     title: "प्रKriti",
     description:
-      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
-    //image: प्रKriti,
+      "प्रKriti is a sustainability-driven platform using AI for waste segregation and green retail, built collaboratively for SMART INDIA HACKATHON 2024 Grand Finale.",
+    image: prakriti,
     tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
     github: "https://github.com/codingmastr/CSPrep",
-    webapp: "https://csprep.netlify.app/",
+    webapp: "https://683dbeba67cffe7312ab98e0--lambent-gingersnap-9d3d6e.netlify.app/",
   },
   {
     id: 2,
     title: "FeedMeBack",
     description:
       "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
-    //image: 
+    image: feedMeBack,
     tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-    github: "https://github.com/codingmastr/Movie-Recommendation-App",
-    webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    github: "https://github.com/geekyanimesh/FeedMeBack",
+   // webapp: 
   },
 ];
