dd4492c436943e59b1277ebd9918244e30c30c80
geekyanimesh <animesh.kumar9651@gmail.com>
2025-12-03 12:51:02 +0530

    resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1aDvMAg8NhUxRgFcO_d8Jhg9r6JhXaPWP/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Uf2Nzo52bRn3Yy1qfw34efZu37dqbQrz/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
