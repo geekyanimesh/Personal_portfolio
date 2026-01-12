2dfea72794871a2e072328888f124befb19e2d30
geekyanimesh <animesh.kumar9651@gmail.com>
2026-01-12 19:20:21 +0530

    added resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1Uf2Nzo52bRn3Yy1qfw34efZu37dqbQrz/view?usp=sharing"
+            href="https://drive.google.com/file/d/1XnWMHcLM1Sqv9TceeAMxCY-6BOJ2vM5a/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
